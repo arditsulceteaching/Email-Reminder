6674a475b17739a65c011306283c0152580b2bde
arditsulceteaching <arditsulce@gmail.com>
2025-05-23 10:48:23 +0100

    Create the email message

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 import csv
+import smtplib
+from email.message import EmailMessage
 
 with open("reminders.csv") as file:
-    reader = csv. DictReader(file)
+    reader = csv.DictReader(file)
     reminders = list(reader)
 
 print(reminders)
 
+for r in reminders:
+    msg = EmailMessage()
+    msg['Subject'] = "⏰ Your Reminder for Today"
+    msg['From'] = "app10flask@gmail.com"
+    msg['To'] = r['email']
+    msg.set_content(f"Message: {r['message']}\n"
+                    f"Date: {r['date']}\n"
+                    f"Time: {r['time']}")
+    print(msg)
