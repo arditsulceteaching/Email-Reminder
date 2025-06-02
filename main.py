b56844d93f2d16f0b09f735be99f36be7bb0b4e9
arditsulceteaching <arditsulce@gmail.com>
2025-06-02 11:37:18 +0100

    Optimize code

--- main.py
+++ main.py
@@ -10,8 +10,6 @@ load_dotenv()
 email_address = os.getenv("EMAIL_ADDRESS")
 email_password = os.getenv("EMAIL_PASSWORD")
 
-print(email_address)
-print(email_password)
 
 with open("reminders.csv") as file:
     reader = csv.DictReader(file)
@@ -19,23 +17,26 @@ with open("reminders.csv") as file:
 
 remaining = []
 
-for r in reminders:
-    if r['date'] == datetime.now().strftime("%Y-%m-%d"):
-        msg = EmailMessage()
-        msg['Subject'] = "⏰ Your Reminder for Today"
-        msg['From'] = "Ardit Sulce"
-        msg['To'] = r['email']
-        msg.set_content(f"Message: {r['message']}\n"
-                        f"Date: {r['date']}\n"
-                        f"Time: {r['time']}")
-        with smtplib.SMTP("smtp.gmail.com", 587) as smtp:
-            smtp.starttls()
-            smtp.login(email_address, email_password)
-            smtp.send_message(msg)
-    else:
-        remaining.append(r)
+today = datetime.now().strftime("%Y-%m-%d")
 
-print(remaining)
+with smtplib.SMTP("smtp.gmail.com", 587) as smtp:
+    smtp.starttls()
+    smtp.login(email_address, email_password)
+
+    for r in reminders:
+        if r['date'] == today:
+            msg = EmailMessage()
+            msg['Subject'] = "⏰ Your Reminder for Today"
+            msg['From'] = "Ardit Sulce"
+            msg['To'] = r['email']
+            msg.set_content(f"Message: {r['message']}\n"
+                            f"Date: {r['date']}\n"
+                            f"Time: {r['time']}")
+
+            smtp.send_message(msg)
+        else:
+            remaining.append(r)
+
 
 with open("reminders.csv", "w") as file:
     writer = csv.DictWriter(file,
